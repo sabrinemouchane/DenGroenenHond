862fee57a2e88ba6d3434aef83ddab548cede42a
Je Echte Naam <jouw.email@voorbeeld.com>
2026-01-29 14:42:39 +0100

    Transport module toegevoegd

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
     private static CustomerService customerService = new CustomerService();
     private static EmployeeService employeeService = new EmployeeService();
     private static PlantService plantService = new PlantService();
